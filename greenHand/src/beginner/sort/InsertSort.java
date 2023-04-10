70fb6a06a855efadd895e01886044abeae730bed
denn <715545794@qq.com>
2023-04-10 20:29:49 +0800

    优化插入排序

--- greenHand/src/beginner/sort/InsertSort.java
+++ greenHand/src/beginner/sort/InsertSort.java
@@ -4,8 +4,28 @@ public class InsertSort {
     public static void main(String[] args) {
         int[] arr = new int[]{46, 5, 7, 10, 8, 6, 9, 3, 0, 2, 4, 1, 5};
         printArr(arr);
-        sort(arr);
+        OptimizeSort(arr);
         printArr(arr);
+    }
+
+    /**
+     * 优化插入排序
+     *
+     * @param arr
+     */
+    private static void OptimizeSort(int[] arr) {
+        if (arr == null || arr.length == 1) {
+            return;
+        }
+        // 0 ~ 0
+        // 0 ~ 1
+        // 0 ~ n-1
+        int N = arr.length;
+        for (int end = 1; end <= N - 1; end++) {
+            for (int pre = end - 1; pre >= 0 && arr[pre] > arr[pre + 1]; pre--) {
+                swap(arr, pre, pre + 1);
+            }
+        }
     }
 
     private static void sort(int[] arr) {
