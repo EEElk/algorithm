cb2a8fcf7cd69d547f15d3beef0d4a2b1ed8efe5
denn <715545794@qq.com>
2023-04-22 22:07:44 +0800

    新手班16 - 有序数组中找到num（最基础的二分法）

--- 算法与数据结构新手班/src/class03/BinarySearchWithSort.java
+++ 算法与数据结构新手班/src/class03/BinarySearchWithSort.java
@@ -30,20 +30,19 @@ public class BinarySearchWithSort {
     }
 
 
-
     public static int findNumIndex(int[] arr, int num) {
         if (arr == null || arr.length == 0) {
             return -1;
         }
         int l = 0;
         int r = arr.length - 1;
-        while(l <= r){
+        while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (arr[mid] == num){
+            if (arr[mid] == num) {
                 return mid;
             } else if (arr[mid] < num) {
                 l = mid + 1;
-            }else {
+            } else {
                 r = mid - 1;
             }
         }
@@ -53,10 +52,8 @@ public class BinarySearchWithSort {
     /**
      * 生成随机数组 - 用于测试算法正确性
      *
-     * @param maxLenth
-     *            随机数组的最大长度，范围在 1 ~ maxLength
-     * @param maxValue
-     *            随机数组每个元素最大值, 范围在 1 ~ maxValue
+     * @param maxLenth 随机数组的最大长度，范围在 1 ~ maxLength
+     * @param maxValue 随机数组每个元素最大值, 范围在 1 ~ maxValue
      * @return 生成的随机数组
      */
     public static int[] generateRandomArray(int maxLenth, int maxValue) {
