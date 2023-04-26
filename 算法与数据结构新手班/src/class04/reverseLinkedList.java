2d15413cb8b55bf293217b156b262bad8307e3c6
denn <715545794@qq.com>
2023-04-26 13:21:50 +0800

    新手班26 - 单链表的反转

---
+++ 算法与数据结构新手班/src/class04/reverseLinkedList.java
@@ -0,0 +1,28 @@
+package class04;
+
+/**
+ * 单链表的反转
+ */
+public class reverseLinkedList {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            value = data;
+        }
+    }
+
+    public static Node reverseLinkedList(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
