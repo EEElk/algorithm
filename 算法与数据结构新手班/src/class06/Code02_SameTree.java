5ac34d74aac1690b7510004f84cee2e075acce07
denn <715545794@qq.com>
2023-05-11 10:15:23 +0800

    新手班44 - 判断两颗树结构是否相同

---
+++ 算法与数据结构新手班/src/class06/Code02_SameTree.java
@@ -0,0 +1,20 @@
+package class06;
+
+public class Code02_SameTree {
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        // 异或,p和q不同则为true
+        if (p == null ^ q == null){
+            return false;
+        }
+        if (p == null && q == null){
+            return true;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
