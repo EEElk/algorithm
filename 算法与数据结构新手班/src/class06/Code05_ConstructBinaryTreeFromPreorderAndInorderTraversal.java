bd1e2fc368143e465434ff5863c6fe3600e3ff46
denn <715545794@qq.com>
2023-05-15 14:14:32 +0800

    新手班47 - 从前序与中序遍历序列构造二叉树

---
+++ 算法与数据结构新手班/src/class06/Code05_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,117 @@
+package class06;
+
+import java.util.HashMap;
+
+/**
+ * 从前序与中序遍历序列构造二叉树
+ * 前序[头,左,右] 中序[左,头,右]
+ */
+public class Code05_ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 方法f:采用遍历来确认前序数组和中序数组中根节点的位置
+     */
+    public static TreeNode buildTree1(int[] pre, int[] in) {
+        if (pre == null || in == null || pre.length != in.length) {
+            return null;
+        }
+        return f(pre, 0, pre.length - 1, in, 0, in.length - 1);
+    }
+
+    // 有一棵树，先序结果是pre[L1...R1]，中序结果是in[L2...R2]
+    // 请建出整棵树返回头节点
+    public static TreeNode f(int[] pre, int L1, int R1, int[] in, int L2, int R2) {
+        if (L1 > R1) {
+            return null;
+        }
+        TreeNode head = new TreeNode(pre[L1]);
+        if (L1 == R1) {
+            return head;
+        }
+        int find = L2;
+        while (in[find] != pre[L1]) {
+            find++;
+        }
+        // 前序[头,左,右] 中序[左,头,右]
+        // R1=L1+(R2-L2)=L1+1+find-1+L2=L1+find-L2
+        head.left = f(pre, L1 + 1, L1 + find - L2, in, L2, find - 1);
+        head.right = f(pre, L1 + find - L2 + 1, R1, in, find + 1, R2);
+        return head;
+    }
+
+    public static TreeNode buildTree2(int[] pre, int[] in) {
+        if (pre == null || in == null || pre.length != in.length) {
+            return null;
+        }
+        HashMap<Integer, Integer> valueIndexMap = new HashMap<>();
+        for (int i = 0; i < in.length; i++) {
+            valueIndexMap.put(in[i], i);
+        }
+        return g(pre, 0, pre.length - 1, in, 0, in.length - 1, valueIndexMap);
+    }
+
+    // 有一棵树，先序结果是pre[L1...R1]，中序结果是in[L2...R2]
+    // 请建出整棵树返回头节点
+    public static TreeNode g(int[] pre, int L1, int R1, int[] in, int L2, int R2,
+                             HashMap<Integer, Integer> valueIndexMap) {
+        if (L1 > R1) {
+            return null;
+        }
+        TreeNode head = new TreeNode(pre[L1]);
+        if (L1 == R1) {
+            return head;
+        }
+        int find = valueIndexMap.get(pre[L1]);
+        head.left = g(pre, L1 + 1, L1 + find - L2, in, L2, find - 1, valueIndexMap);
+        head.right = g(pre, L1 + find - L2 + 1, R1, in, find + 1, R2, valueIndexMap);
+        return head;
+    }
+
+    // 打印二叉树的函数
+    public static void printTree(TreeNode head) {
+        System.out.println("打印二叉树，看的时候请把图像顺指针转90度:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+    // 打印二叉树的函数
+    public static void printInOrder(TreeNode head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.val + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+    // 打印二叉树的函数
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] pre = {1, 2, 3, 4, 5, 6, 7};
+        int[] in = {4, 3, 2, 5, 1, 6, 7};
+        TreeNode head = buildTree1(pre, in);
+        printTree(head);
+    }
+}
